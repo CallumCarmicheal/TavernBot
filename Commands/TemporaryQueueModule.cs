9247560bacebbf3b29de3f0fbd3cfbd3ef610321
Callum Carmicheal <callumcarmicheal@gmail.com>
2023-08-20 17:19:30 +0100

    Fixed compiling issue

--- Commands/TemporaryQueueModule.cs
+++ Commands/TemporaryQueueModule.cs
@@ -13,6 +13,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using DSharpPlus.Interactivity.Extensions;
 
 namespace CCTavern.Commands {
     internal class TemporaryQueueModule : BaseCommandModule {
